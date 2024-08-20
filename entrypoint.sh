aff6b22d909343394e4da172aae329c640cf9973
imaneelfilali <elfilali.imane02@gmail.com>
2024-08-20 09:20:32 +0100

    edit

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 # create a db and a table:
 echo "Initializing database..."
 mysql -u root -pmysql <<-EOSQL
-CREATE DATABASE IF NOT EXISTS my_database;
-USE my_database;
+CREATE DATABASE IF NOT EXISTS springboot_db;
+USE springboot_db;
 CREATE TABLE IF NOT EXISTS student (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
